ae5a1653dc81b5ef2767ecc74ca7e3f396b2952f
lostvagabond <115056039+lostvagabond@users.noreply.github.com>
2022-12-07 23:04:53 -0500

    Create choicemaker.py

---
+++ choicemaker.py
@@ -0,0 +1,25 @@
+# This is a really simple program that makes a choice when one is feeling indecisive
+# It starts by asking how many choices there are, then it asks you to list the choices
+# using a comma. When that is done, it randomly selects a choice for you
+
+import random
+print("welcome")
+print("how many options are we dealing with?")
+amount = input()
+if amount.isdigit():
+    amount = int(amount)
+else:
+    print("Choose a number with no spaces next time")
+    quit()
+print("What %d options would you like to randomize? separate them using commas" % amount)
+options = input()
+list1 = options.split(",")
+if len(list1)<amount:
+    print("insufficient text")
+    quit()
+elif len(list1)>amount:
+    print("too much text")
+    quit()
+print("we will now make a random choice for you")
+choice = random.randrange(amount)
+print(list1[choice])
