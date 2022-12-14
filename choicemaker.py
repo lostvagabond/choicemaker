41411d04e1e6dbdf8e17b46db3eb3cc1a1930f05
Cristopher Serratos <115056039+lostvagabond@users.noreply.github.com>
2022-12-13 19:14:39 -0500

    Update choicemaker.py

--- choicemaker.py
+++ choicemaker.py
@@ -3,23 +3,30 @@
 # using a comma. When that is done, it randomly selects a choice for you
 
 import random
+
 print("welcome")
+
 print("how many options are we dealing with?")
 amount = input()
+
 if amount.isdigit():
     amount = int(amount)
 else:
     print("Choose a number with no spaces next time")
     quit()
+    
 print("What %d options would you like to randomize? separate them using commas" % amount)
 options = input()
 list1 = options.split(",")
+
 if len(list1)<amount:
     print("insufficient text")
     quit()
 elif len(list1)>amount:
     print("too much text")
     quit()
+    
 print("we will now make a random choice for you")
+
 choice = random.randrange(amount)
 print(list1[choice])
